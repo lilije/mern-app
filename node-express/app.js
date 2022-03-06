fe4301c49c81a398816ca5ee6266dfe756a084ff
lilizhang <lilizhang254@gmail.com>
2022-03-06 14:44:24 -0800

    add specific place routing

--- node-express/app.js
+++ node-express/app.js
@@ -1,10 +1,24 @@
-const fs = require('fs');//file system
-const userName = 'Mak';
+const http = require('http');
 
-fs.writeFile('user-data.txt', 'Name:' + userName, (err) => {
-    if (err) {
-        console.log(err);
-        return;
+const server = http.createServer((req, res) => {
+    console.log('incoming request');
+    console.log(req.method, req.url);
+
+    if (req.method === 'POST') {
+        let body = '';
+        req.on(('end', () => {
+            const userName = body.split('=')[1];
+            res.end('<h1>' + useerName + '</h1>');
+        }));
+        req.on('data', (chunk) => {
+            body += chunk;
+        });
+        
+    } else {
+        // res.setHeader('Content-Type', 'text/html');
+        res.end('<form method="POST"><input type="text" name="username" /><button type="subimt">Create user</button></form>');
     }
-    console.log('write file');
-} )
+    
+});
+
+server.listen(5000);//open a local server 
